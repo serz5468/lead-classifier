b1d92075e485dbdb154fef9d040e7e9ac9e7a4e6
serz5468 <shellowserafim@gmail.com>
2026-05-19 21:31:37 +0200

    Add files via upload

---
+++ lead_classifier.py
@@ -0,0 +1,22 @@
+leads = [ {'imie': "Kacper", 'budzet': 499},
+       {'imie': "Anna", 'budzet': 698},
+       {'imie': "Bartosz", 'budzet': 476},
+       {'imie': "Kuba", 'budzet': 825},
+       {'imie': "Oliwia", 'budzet': 655},
+       {'imie': "Jagoda", 'budzet': 900},
+       {'imie': "Marek", 'budzet': 303}
+]
+def classify_lead(budzet):
+    if budzet < 500:
+        return("COLD")
+    elif 500 <= budzet <= 700:
+        return("WARM")
+    else:
+        return("HOT")
+for lead in leads:
+    wynik = classify_lead(lead['budzet'])
+    print(lead['imie'], "->", wynik )
+
+    
+
+ 
